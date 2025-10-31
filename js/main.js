226c923851b141b73ff1941ab976d617898f9fb4
ThalesLimaF <thalesferreiraalima@gmail.com>
2025-10-31 15:39:55 -0300

    bandeiras

---
+++ js/main.js
@@ -0,0 +1,14 @@
+import flags from './model/flags.js';
+
+const conteudo = document.querySelector('main');
+
+flags.forEach(flag => {
+  const card = `
+  <div class="flag col-2 my-2 text-center">
+  <img src="${flag.image}" alt="${flag.name}">
+  <p>${flag.name}</p>
+  </div>`;
+
+conteudo.insertAdjacentHTML("beforeend", card);
+})
+
